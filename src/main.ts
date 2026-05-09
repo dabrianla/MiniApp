f8f93fc1c668fc0ed2989e4c396b35beaf9d3e7f
dabrianla <81344554+dabrianla@users.noreply.github.com>
2026-05-09 14:03:41 -0400

    firebase listo

--- src/main.ts
+++ src/main.ts
@@ -4,11 +4,13 @@ import { IonicRouteStrategy, provideIonicAngular } from '@ionic/angular/standalo
 
 import { routes } from './app/app.routes';
 import { AppComponent } from './app/app.component';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 
 bootstrapApplication(AppComponent, {
   providers: [
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
     provideIonicAngular(),
-    provideRouter(routes, withPreloading(PreloadAllModules)),
+    provideRouter(routes, withPreloading(PreloadAllModules)), provideFirebaseApp(() => initializeApp({ projectId: "miniapp-inventario", appId: "1:633727289203:web:01c607e63eeef7b8412706", storageBucket: "miniapp-inventario.firebasestorage.app", apiKey: "AIzaSyCmfN1TZ5m10zxYiX5TyqTM7E3rfRoeihw", authDomain: "miniapp-inventario.firebaseapp.com", messagingSenderId: "633727289203", projectNumber: "633727289203", version: "2" })), provideFirestore(() => getFirestore()),
   ],
 });
